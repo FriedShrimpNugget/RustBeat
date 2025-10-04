75bd950f1a8d48e650e83029f2289611a3822286
orangePrint("ark") <orangeprint908@gmail.com>
2025-10-05 01:19:41 +0200

    Adding more stuff

--- src/components/Play.tsx
+++ src/components/Play.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Song from "./Song";
 export const content1 : React.ReactNode = (<div>
-  <Song artist="xi" song="Happy end of the world"/>
+  <Song artist="No One" song="Example"/>
+  <Song artist="xi" song="Happy end of the world (please someone help me map this)"/>
   <Song artist="You" song="Fork and make more songs , lol"/>
 </div>)
